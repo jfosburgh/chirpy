616d590682a891aa5f4d9f83dc87cddd4159bd31
James Fosburgh <jwfosburgh@gmail.com>
2024-01-17 10:14:56 -0500

    did the next few parts

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module jfosburgh/chirpy
 
 go 1.21.6
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -18,18 +33,40 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func appHandler(w http.ResponseWriter, req *http.Request) {
+func (cfg *apiConfig) metricResetHandler(w http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) metricHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type:", "text/html")
+	page, _ := fs.ReadFile(os.DirFS("./"), "admin/index.html")
+	w.Write([]byte(fmt.Sprintf(string(page), cfg.fileserverHits)))
+}
+
+func healthHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(200)
 	w.Header().Add("Content-Type:", "text/plain; charset=utf-8")
 	w.Write([]byte("OK"))
 }
 
 func main() {
-	mux := http.NewServeMux()
-	corsMux := middlewareCors(mux)
+	r := chi.NewRouter()
+	api := chi.NewRouter()
+	admin := chi.NewRouter()
+	corsMux := middlewareCors(r)
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", appHandler)
+	cfg := apiConfig{0}
+	r.Handle("/app", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	r.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	api.Get("/healthz", healthHandler)
+	admin.Get("/metrics", cfg.metricHandler)
+	api.HandleFunc("/reset", cfg.metricResetHandler)
+	r.Mount("/api", api)
+	r.Mount("/admin", admin)
+
 	server := http.Server{Addr: "localhost:8080", Handler: corsMux}
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		fmt.Printf("ListenAndServe: %v", err)
